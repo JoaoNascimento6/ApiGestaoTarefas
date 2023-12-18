8d6029d1ec3414a3e23a48ad4b6e6f3322916d30
João Nascimento <joao_nascimento6@hotmail.com>
2023-12-18 16:33:58 +0000

    123

--- ToDov3/index.js
+++ ToDov3/index.js
@@ -1,22 +1,19 @@
 'use strict';
 
-var path = require('path');
-var http = require('http');
-var express = require('express');
-var jwt = require('express-jwt');
-var jwksRsa = require('jwks-rsa');
+const path = require('path');
+const http = require('http');
+const express = require('express');
 const { auth } = require('express-openid-connect');
+require('dotenv').config();
 const mongoose = require('mongoose');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://admin:12345@apitodo.twkieed.mongodb.net/?retryWrites=true&w=majority";
 const session = require('express-session');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
+
 const swaggerDocument = YAML.load(path.join(__dirname, 'api/openapi.yaml'));
 
-var serverPort = 8080;
-
-// Configuração do Auth0
 const config = {
   authRequired: false,
   auth0Logout: true,
@@ -24,54 +21,60 @@ const config = {
   baseURL: 'http://localhost:8080',
   clientID: '55iZS7n1jGhAuu55pliaR3WodlIDw2EE',
   issuerBaseURL: 'https://dev-t2451ya5jqel67p5.us.auth0.com',
+  routes: {
+    login: false,
+    callback: '/callback',
+    postLogoutRedirect: '/logout',
+  },
 };
 
-// Crie o app Express
-var app = express();
+const app = express();
 
-// Use session para armazenar dados de sessão
+app.get('/callback', async (req, res, next) => {
+  const { oidc } = req;
+
+  try {
+    await oidc.authenticate({
+      successRedirect: '/rota-apos-login',
+      failureRedirect: '/',
+    })(req, res, next);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get('/rota-apos-login', (req, res) => {
+  res.send('Você está logado!');
+});
+
+const PORT = process.env.PORT || 8080;
+
+http.createServer(app).listen(PORT, () => {
+  console.log(`Seu servidor está ouvindo na porta ${PORT} (http://localhost:${PORT})`);
+  console.log(`Swagger-ui está disponível em http://localhost:${PORT}/api-docs`);
+});
+
 app.use(session({
   secret: '55iZS7n1jGhAuu55pliaR3WodlIDw2EE',
   resave: true,
   saveUninitialized: false,
 }));
 
-// Use o middleware Auth0
 app.use(auth(config));
 
-// Middleware para desativar autenticação em ambiente de desenvolvimento
 app.use((req, res, next) => {
-  if (process.env.NODE_ENV === 'development') {
-    // Desativar autenticação em ambiente de desenvolvimento
-    return next();
-  }
-
-  if (req.oidc.isAuthenticated()) {
+  if (process.env.NODE_ENV === 'development' || req.oidc.isAuthenticated()) {
     return next();
   }
   res.send('Unauthorized');
 });
 
-
-// Configuração do Swagger
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-// Inicializar o servidor HTTP
-http.createServer(app).listen(serverPort, function () {
-  console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
-  console.log('Swagger-ui is available on http://localhost:%d/api-docs', serverPort);
-});
-
-// Conectar ao MongoDB
 mongoose.connect(uri)
-  .then(() => {
-    console.log('connected to MongoDB');
-  })
-  .catch(() => {
-    console.log('error connecting to MongoDB');
-  });
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(() => console.log('Error connecting to MongoDB'));
 
-// Conectar ao MongoDB usando MongoClient
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
